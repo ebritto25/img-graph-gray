f11b752bb290129af6ac0589888a5e4330e254d5
rodrigo <rodrigovalente1996@gmail.com>
2017-10-14 11:32:25 -0300

    mod

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ int main(int argc, char* argv[])
 
     // Caso especial base UCMERCED
         ifstream File;
-        File.open("/media/rodrigo/A96C-4A45/Kylberg/folders_name.txt");
+        File.open("../bases/Kylberg/folders_name.txt");
         if(!File.is_open())
         {
             cerr << "Problema ao Abrir Nomes das Pastas" << '\n';
