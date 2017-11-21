1a2ae021a39c59feafceb680b1055660e99a922a
rodrigo <rodrigovalente1996@gmail.com>
2017-11-21 12:01:45 -0200

    Adicionando Script Para Converter Imagens '.tiff' para '.png'

--- scripts/tensor_flow/tiff_to_png.py
+++ scripts/tensor_flow/tiff_to_png.py
@@ -1,19 +1,24 @@
 import os
+import sys
 import glob
 from PIL import Image
 
 
-path = os.getcwd()
+if len(sys.argv) < 2:
+	print 'Caminho Nao Informado!\n Informar Caminho como argumento'
+else:
+	path = sys.argv[1] 
 
-for image_path in glob.glob(path+'/*.tiff'):
-	png_file = image_path.split('.')[0] + '.png'
 
-	print png_file
-	
-	try:
-		im = Image.open(image_path) 
-		im.thumbnail(im.size)
-		im.save(png_file,"PNG",quality=100)
+	for image_path in glob.glob(path+'/*.tiff'):
+		png_file = image_path.split('.')[0] + '.png'
 
-	except Exception, e:
-		print e 
+		print png_file
+		
+		try:
+			im = Image.open(image_path) 
+			im.thumbnail(im.size)
+			im.save(png_file,"PNG",quality=100)
+
+		except Exception, e:
+			print e 
