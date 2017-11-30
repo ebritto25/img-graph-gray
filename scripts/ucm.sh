6bf9d8614b1923a89a1a87b88add969a59af42fb
Rodrigo Valente Bernardes <rodrigovalente@gmail.com>
2017-11-30 12:26:34 -0200

    Script UCM

--- scripts/ucm.sh
+++ scripts/ucm.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-
+#!/bin/bash 
 base_path="/home/rodrigo/ic/bases/ucm/Images/"
 exec_path="/home/rodrigo/ic/build-img-graph-gray-Desktop-Debug/"
 arff_path="/home/rodrigo/ic/resultados_script/ucm/"
@@ -10,22 +9,33 @@ exec_name="img-graph-gray"
 base_name="ucm"
 mst=1
 color=0 # 0 -> GRAY | 1 -> RGB
-codec=".tiff"
+codec=".tif"
 number_of_images=100
 
 
 
-# Com mst
-$exec_path$exec_name $base_path$base_name $number_of_images $codec $color $mst $arff_path$base_name".arff" 
-echo $exec_path$exec_name $base_path$base_name $number_of_images $codec $color $mst $arff_path$base_name".arff" 
+# Com mst GRAY
+echo $exec_path$exec_name $base_path$base_name $number_of_images $codec $color $mst $arff_path$base_name"_mst.arff" 
+$exec_path$exec_name $base_path $base_name $number_of_images $codec $color $mst $arff_path$base_name"_mst.arff" 
 
 
-# Sem mst
+# Sem mst GRAY
 mst=0
-$exec_path$exec_name $base_path$class $base_name $number_of_images $codec $color $mst $arff_path$base_name".arff" 
-echo $exec_path$exec_name $base_path$base_name $number_of_images $codec $color $mst $arff_path$base_name".arff" 
+echo $exec_path$exec_name $base_path $base_name $number_of_images $codec $color $mst $arff_path$base_name".arff" 
+$exec_path$exec_name $base_path $base_name $number_of_images $codec $color $mst $arff_path$base_name".arff" 
 
 
+color=1
+
+# Com mst RGB
+echo $exec_path$exec_name $base_path$base_name $number_of_images $codec $color $mst $arff_path$base_name"_mst_rgb.arff" 
+$exec_path$exec_name $base_path $base_name $number_of_images $codec $color $mst $arff_path$base_name"_mst_rgb.arff" 
+
+
+# Sem mst RGB
+mst=0
+echo $exec_path$exec_name $base_path $base_name $number_of_images $codec $color $mst $arff_path$base_name"_rgb.arff" 
+$exec_path$exec_name $base_path $base_name $number_of_images $codec $color $mst $arff_path$base_name"_rgb.arff" 
 
 
 
